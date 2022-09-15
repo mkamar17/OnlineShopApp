eb5eb93d38b56b7de690dfc219a80df3d8cab411
Maryam Kamar <maryamkamar@icloud.com>
2022-09-15 14:27:21 +0100

    online shop app

---
+++ app/src/main/java/com/treehouse/onlineshopapp/MainActivity.java
@@ -0,0 +1,14 @@
+package com.treehouse.onlineshopapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
