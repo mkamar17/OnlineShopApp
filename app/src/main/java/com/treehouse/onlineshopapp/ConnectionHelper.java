802a5f92b133c2c17c16590324921e50a627f422
Maryam Kamar <maryamkamar@icloud.com>
2022-09-16 20:48:11 +0100

    online shop app

---
+++ app/src/main/java/com/treehouse/onlineshopapp/ConnectionHelper.java
@@ -0,0 +1,12 @@
+package com.treehouse.onlineshopapp;
+
+import java.sql.Connection;
+
+public class ConnectionHelper {
+    Connection con;
+    String uname, pass, ip, port, database;
+
+    public Connection connectionclass() {
+        ip = ""
+    }
+}
