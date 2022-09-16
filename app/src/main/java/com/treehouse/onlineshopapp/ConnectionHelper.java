f8d69023561357c48b146951c9ff81e3d02e8f75
Maryam Kamar <maryamkamar@icloud.com>
2022-09-16 21:17:16 +0100

    online shop app

--- app/src/main/java/com/treehouse/onlineshopapp/ConnectionHelper.java
+++ app/src/main/java/com/treehouse/onlineshopapp/ConnectionHelper.java
@@ -1,8 +1,10 @@
 package com.treehouse.onlineshopapp;
 
 import android.os.StrictMode;
+import android.util.Log;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 
 public class ConnectionHelper {
     Connection con;
@@ -15,6 +17,20 @@ public class ConnectionHelper {
         pass="reallyStrongPwd123";
         port="1433";
 
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy()
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL = null;
+
+        try {
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            ConnectionURL="jdbc.jtds:sqlserver://" + ip + ":" + port + ";" + "databasename=" + database+";user="+uname+";password="+pass+";";
+            connection = DriverManager.getConnection(ConnectionURL);
+        }
+        catch (Exception ex) {
+            Log.e("Error ", ex.getMessage());
+        }
+
+        return connection;
     }
 }
