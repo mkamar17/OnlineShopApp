eb9461a9180e86224cbe8075dca5ea9b8d612674
Maryam Kamar <maryamkamar@icloud.com>
2022-09-19 23:05:12 +0100

    online shop app

--- app/src/main/java/com/treehouse/onlineshopapp/MainActivity.java
+++ app/src/main/java/com/treehouse/onlineshopapp/MainActivity.java
@@ -7,45 +7,120 @@ import android.view.View;
 import android.widget.TextView;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
+import android.support.v7.app.ActionBarActivity;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends ActionBarActivtity{
 
-    Connection connect;
-    String ConnectionResult="";
+    // declaring layout button, edit texts
+    public Button run;
+    public TextView message;
+    public ProgressBar progressBar;
+    // ddeclaring connection variables
+    public Connection con;
+    //end declaring connection variables
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //getting values from button, texts and progress bar
+        run = (Button) findViewById(R.id.button);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        //setting up the function when button login is clicked
+        run.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v){
+                CheckLogin checkLogin = new CheckLogin();
+                checkLogin.execute("");
+            }
+        });
+        //end setting up function when button login clicked
     }
 
-    public void GetTextFromSQL (View v){
-        TextView txt1 = (TextView) findViewById(R.id.textView);
-        TextView txt2 = (TextView) findViewById(R.id.textView2);
+    public class CheckLogin extends AsyncTask<String,String,String>{
+        String z = "";
+        Boolean isSuccess = false;
+        String name1 = "";
 
-        try {
-            ConnectionHelper connectionHelper = new ConnectionHelper();
-            connect = connectionHelper.connectionclass();
-            if(connect!=null){
-                String query="Select * from User_Accounts";
-                Statement st= connect.createStatement();
-                ResultSet rs= st.executeQuery(query);
+        protected void onPreExecute(){
+            progressBar.setVisibility(View.VISIBLE);
 
-                while (rs.next()){
-                    txt1.setText(rs.getString(1));
-                    txt2.setText(rs.getString(2));
+        }
+
+        @Override
+        protected void onPostExecute(String r){
+            progressBar.setVisibility(View.GONE);
+            Toast.makeText(MainActivity.this,r,Toast.LENGTH_LONG).show();
+            if(isSuccess){
+                message = (TextView) findViewbyId(R.id.textView2);
+                message.setText(name1);
+            }
+        }
+
+        @Override
+        protected String doInBackground(String... params){
+            try{
+                con =connectionclass(); //connect to database
+                if (con == null){
+                    z = "check internet access!";
+                }
+                else{
+                    //change below query according to your own database
+                    String query = "select * from UserAccounts";
+                    Statement stmt.executeQuery(query);
+                    if(rs.next()){
+                        name1 = rs.getString("UserName"); //name is the string of a column in the database, read through
+                        z = "query successful";
+                        isSuccess=true;
+                        con.close();
+                    }
+                    else{
+                        z = "invalid query";
+                        isSuccess=false;
+                    }
                 }
             }
-            else {
-                ConnectionResult="Check connection";
+            catch (Exception ex){
+                isSuccess = false;
+                z = ex.getMessage();
+
+                Log.d("sql error", z);
             }
 
+            return z;
         }
-        catch (Exception ex){
 
-        }
     }
 
+    @SuppressLint("NewApi")
+    public Connection connectionclass() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL = null;
+
+        try{
+            Class.forName("net.sourceforge.jtds.jdbc.Driver");
+            //your database connection string goes below
+            ConnectionURL="";
+            connection = DriveManager.getConnection(ConnectionURL);
+        }
+        catch(SQLException se){
+            Log.e("error here 1: ", set.getMessage());
+        }
+        catch(ClassNotFoundException e){
+            Log.e("error here 2: ", e.getMessage());
+        }
+        catch(Exception e){
+            Log.e("error here 3: ",e.getMessage());
+        }
+        return connection;
+    }
 
 }
